7df2668ff6649244c6ed98abf4951110eed9b131
Shubham Kumar Gupta <gptshubham595@gmail.com>
2019-07-22 20:17:04 +0530

    Before forgot

---
+++ app/src/main/java/com/codefundo/vote/ExitActivity.java
@@ -0,0 +1,4 @@
+package com.codefundo.vote;
+
+class ExitActivity {
+}
---
+++ app/src/main/java/com/codefundo/vote/Forgot.java
@@ -0,0 +1,14 @@
+package com.codefundo.vote;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Forgot extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot);
+    }
+}
